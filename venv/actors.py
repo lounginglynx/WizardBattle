2ab79c155bb8289bc3ac0de1c38c61f7f27a0c6d
lounginglynx <acmonnin@gmail.com>
2019-10-02 22:18:19 -0400

    Commit Build

---
+++ venv/actors.py
@@ -0,0 +1,5 @@
+class Creature:
+    pass
+
+class Wizard:
+    pass
--- venv/program.py
+++ venv/program.py
@@ -1,15 +1,43 @@
+from actors import Wizard, Creature
 
 def main():
     header()
+    intro()
     gameLoop()
 
 def header():
     print('---------------------------------------------')
     print('           Wizard Wars')
     print('---------------------------------------------')
+    print()
+
+def intro():
+    print()
+    print('A long time ago in a galaxy far far away.....')
+    print('We find our young wizard wandering the enchanted forest')
 
 def gameLoop():
-    pass
+    creatures = [
+        Creature(),
+        Creature(),
+        Creature()
+    ]
+    hero = Wizard()
+
+
+    while True:
+        cmd = input("Do you [a]ttack, [r]un away, or [l]ook around?")
+        if cmd == 'a':
+            print('Wands Drawn')
+        elif cmd == 'r':
+            print('Harry sprints off into the woods')
+        elif cmd == 'l':
+            print('Harry looks around for a good pub')
+        else:
+            print('Harry decides he has had enough. And so he reaches around to his back pocket and pulls out a mysterious brown packet')
+            print('He flings out a magic bean and throws it into the earth. Quickly he scampers up the fast growing beanstalk to saftey')
+            break
+
 
 
 
