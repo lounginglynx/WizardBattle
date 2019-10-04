51acb7ff0b03f439302d71ce6045f510b1cf0113
lounginglynx <acmonnin@gmail.com>
2019-10-03 22:10:50 -0400

    Commit Build

--- venv/actors.py
+++ venv/actors.py
@@ -1,9 +1,9 @@
-
+import random
 
 class Creature:
-    def __init__(self, Name, Level):
-        self.name = Name
-        self.level = Level
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
 
     def __repr__(self):
         return "Creature {} of level {}".format(
@@ -11,9 +11,29 @@ class Creature:
             self.level
 
         )
+    def get_defensive_roll(self):
+        return random.randint(1,12) * self.level
+
 
 class Wizard:
     def __init__(self, name, level):
         self.name = name
         self.level= level
+
+    def attack(self, creature):
+        print('{} draws his wand and attacks {}'.format(self.name, creature.name))
+
+        my_roll = random.randint(1,11) * self.level
+        #creature_roll = random.randint(1,12) * creature.level
+        creature_roll = creature.get_defensive_roll()
+
+        print('You roll {}..'.format(my_roll))
+        print('{} rolls {}'.format(creature.name, creature_roll))
+
+        if my_roll >= creature_roll:
+            print('Harry has won this time')
+            return True
+        else:
+            print('Our young wizard has fallen')
+            return False
 
--- venv/program.py
+++ venv/program.py
@@ -1,4 +1,6 @@
 from actors import Wizard, Creature
+import random
+import time
 
 def main():
     header()
@@ -24,21 +26,37 @@ def gameLoop():
         Creature('Voldemort',1000)
     ]
     hero = Wizard('Harry', 75)
-    print(creatures)
+    #print(creatures)
 
     while True:
+
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appeared from the mist'.format(active_creature.name,active_creature.level))
+        print()
+
         cmd = input("Do you [a]ttack, [r]un away, or [l]ook around?")
         if cmd == 'a':
-            print('Wands Drawn')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print('Harry runs off to the bushes to recuperate..')
+                time.sleep(10)
+                print('After finding some butterbeer harry returns stronger then ever')
         elif cmd == 'r':
             print('Harry sprints off into the woods')
         elif cmd == 'l':
-            print('Harry looks around for a good pub')
+            print('Harry looks into the foggy evening and sees:')
+            for c in creatures:
+                print('A {} of level {}'.format(c.name, c.level))
+                print()
         else:
             print('Harry decides he has had enough. And so he reaches around to his back pocket and pulls out a mysterious brown packet')
             print('He flings out a magic bean and throws it into the earth. Quickly he scampers up the fast growing beanstalk to saftey')
             break
 
+        if not creatures:
+            print('Our young wizard has beaten the odds and survived the forest')
+            break
 
 
 
